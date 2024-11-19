4378509d766aad8000d516f38380fe02810259ec
peverill <mrpeverill@gmail.com>
2024-11-19 08:24:24 -0600

    Theoretically more memory efficient brute force solution

--- s8.p2.hs
+++ s8.p2.hs
@@ -8,14 +8,25 @@ lineParse a = (key, (lval, rval))
         lval = take 3 $ drop 7 a
         rval = take 3 $ drop 12 a
 
---ltest :: Char -> [String] -> Bool
---ltest x y = foldl1 (&&) $ map (==x) y
+--basic recursion. This stack overflows with the real data. The code is fine, but the problem needs something more sophisticated than this brute force solution.
+--Also there is a problem with lazy evaluation https://wiki.haskell.org/Foldr_Foldl_Foldl%27
 
 travel :: [String] -> String -> Map String (String, String)-> Int
 travel locs (d:ds) m
   | all (=='Z') (map last locs) = 0
   | otherwise = 1 + travel next ds m
   where mapTuples = map (m Map.!) locs
+        next
+          | d == 'R' = map snd mapTuples
+          | d == 'L' = map fst mapTuples
+          | otherwise = error "Bad Direction"
+
+travel' :: [String] -> String -> Map String (String, String)-> Int -> Int
+travel' locs (d:ds) m n
+  | all (=='Z') (map last locs) = n'
+  | otherwise = seq n' travel' next ds m n'
+  where n' = n + 1
+        mapTuples = map (m Map.!) locs
         next
           | d == 'R' = map snd mapTuples
           | d == 'L' = map fst mapTuples
@@ -28,5 +39,5 @@ main = do
   contents <- getContents
   let cmap = Map.fromList . map lineParse $ drop 1 $ lines contents
   print [x | x<-Map.keys cmap, last x == 'A']
-  let pathCount = travel [x | x<-Map.keys cmap, last x == 'A'] dirs cmap
+  let pathCount = travel' [x | x<-Map.keys cmap, last x == 'A'] dirs cmap 0
   print pathCount
